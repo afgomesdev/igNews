0f5937050ac8418720731f65f822c86f4f15864c
André Felipe Benedetti Gomes <andregomes@pecege.com>
2021-11-20 14:37:06 -0300

    feat: add usern on db or get infos

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,33 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 
+import { fauna } from '../../../services/faunadb';
+
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
       scope: 'read:user',
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      try {
+        const { email } = user;
+        await fauna.query(
+          q.If(
+            q.Not(q.Exists(q.Match(q.Index('user_by_email'), user.email))),
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(q.Match(q.Index('user_by_email'), user.email))
+          )
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
